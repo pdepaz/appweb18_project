ca6b7e6ae9593f9d5c027383021e9cd97fa32d51
Pablo de Paz <100346795@alumnos.uc3m.es>
2018-12-14 19:46:22 +0100

    db

--- apache/apache-tomcat-9.0.12/webapps/proyecto/WEB-INF/src/proyecto/DBManager.java
+++ apache/apache-tomcat-9.0.12/webapps/proyecto/WEB-INF/src/proyecto/DBManager.java
@@ -25,6 +25,7 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.*;
 
 public class DBManager implements AutoCloseable { //Se llama a "close" automaticamente
 
