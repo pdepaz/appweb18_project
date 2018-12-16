60d878a0180fa4a888c4680071cc27be9d66ee94
Pablo <pablo.depaz@hotmail.es>
2018-12-16 19:35:34 +0100

    nueva pelicula files

--- apache/apache-tomcat-9.0.12/webapps/proyecto/WEB-INF/src/proyecto/DBManager.java
+++ apache/apache-tomcat-9.0.12/webapps/proyecto/WEB-INF/src/proyecto/DBManager.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.lang.reflect.Array;
 import java.util.*;
